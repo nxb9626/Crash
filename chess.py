ee76bc5ce8be95fc798413296d37d52851993085
nxb9626 <nxb9626@rit.edu>
2022-02-11 01:17:52 -0500

    [game] swapped to building board from fenstring
    edited how board is printed out a bit
    
    added spaces as intermediary between pieces and board

--- chess.py
+++ chess.py
@@ -2,74 +2,101 @@ import math
 from logging import *
 import enum
 
+black_color = '\033[04m\033[36m' #cyan
+white_color = '\033[95m' #pink
+reset_color = '\033[0m'
+
 class Piece:
-    def __init__(self, team) -> None:
+    def __init__(self, team, board) -> None:
         """
         team should be "0" or "1 #todo update docstring
         """
         self.team = team
+        # self.position = position
+        self.board = board
+    # @abc.abstractclassmethod
+    # def get_potential_moves(self):
+    #     pass
     
 class Pawn(Piece):
     value = 1
-    moves = {}
+    past_positions = []
+    
     def __repr__(self) -> str:
-        if self.team == 0:
-            return "wP"
-        return "bP"
+        if self.team == Team.black:
+            return black_color + "P" + reset_color
+        return white_color+ "p"+ reset_color
+    
+    def get_potential_moves(self):
+        # positions = []
+        # x = self.position[0]
+        # y = self.position[1]
+        # opponents = []
+        # if self.team ==Team.black:
+        #     opponents += [1,3]
+
+        #     if len(self.past_positions) == 0:
+        #         if(self.board.getSpace((x, y+2)).team == 1 or 
+        #             self.board.getSpace((x, y+2)).team == 3):
+        #             positions += (x,y+2)
+
+        return 
+        
+    def add_move(self, move):
+        self.moves += move
 
 class Bishop(Piece):
     value = 3
     moves = {}
-    color = ''
     def __repr__(self) -> str:
-        if self.team == 0:
-            return "wB"
-        return "bB"
+        if self.team == Team.black:
+            return black_color + "B"+ reset_color
+        return white_color+ "b"+ reset_color
 
 class Knight(Piece):
     value = 3
     moves = {}
-    color = ''
     def __repr__(self) -> str:
-        if self.team == 0:
-            return "wK"
-        return "bK"
+        if self.team == Team.black:
+            return black_color +"N"+ reset_color
+        return white_color+ "n"+ reset_color
 
 class Rook(Piece):
     value = 5
     moves = {}
     def __repr__(self) -> str:
-        if self.team == 0:
-            return "wR"
-        return "bR"
+        if self.team == Team.black:
+            return black_color +"R"+ reset_color
+        return white_color+ "r"+ reset_color
 
 class Queen(Piece):
     value = 9
     moves = {}
     def __repr__(self) -> str:
-        if self.team == 0:
-            return "wQ"
-        return "bQ"
+        if self.team == Team.black:
+            return black_color +"Q"+ reset_color
+        return white_color+ "q"+ reset_color
 
 class King(Piece):
     value = math.inf
     move = {}
     def __repr__(self) -> str:
-        if self.team == 0:
-            return "wK"
-        return "bK"
+        if self.team == Team.black:
+            return black_color +"K"+ reset_color
+        return white_color+ "k"+ reset_color
 
 class Empty():
     def __repr__(self) -> str:
-        return "⬛"
+        return " "
 
-class Turn(enum.Enum):
+class Team(enum.Enum):
     white = 0
     black = 1
+    empty = 2
 
 class Move():
     
-    def __init__(self,notation):
+    def __init__(self, notation):
         self.location = notation[-2:]
         if len(notation)>2:
             self.piece = notation[0]
@@ -80,48 +107,93 @@ class Move():
     def __repr__(self) -> str:
         return str(self.piece) + str(self.location)
     
-   
+class Space():
+    def __init__(self, pos, piece) -> None:
+        self.piece = piece
+        self.piece = pos  
 
+    def __repr__(self) -> str:
+        return str(self.piece)
+    
 
-class Board:
-    moves = []
-    moves_without_caputres = 0
-    currentMove = Turn.white
-    grid = [
-        [Rook(1),   Knight(1),  Bishop(1),  Queen(1),   King(1),    Bishop(1),  Knight(1),  Rook(1)],
-        [Pawn(1),   Pawn(1),    Pawn(1),    Pawn(1),    Pawn(1),    Pawn(1),    Pawn(1),    Pawn(1)],
-        [Empty(),   Empty(),    Empty(),    Empty(),    Empty(),    Empty(),    Empty(),    Empty()],
-        [Empty(),   Empty(),    Empty(),    Empty(),    Empty(),    Empty(),    Empty(),    Empty()],
-        [Empty(),   Empty(),    Empty(),    Empty(),    Empty(),    Empty(),    Empty(),    Empty()],
-        [Empty(),   Empty(),    Empty(),    Empty(),    Empty(),    Empty(),    Empty(),    Empty()],
-        [Pawn(0),   Pawn(0),    Pawn(0),    Pawn(0),    Pawn(0),    Pawn(0),    Pawn(0),    Pawn(0)],
-        [Rook(0),   Knight(0),  Bishop(0),  Queen(0),   King(0),    Bishop(0),  Knight(0),  Rook(0)]
-        ]
+class Board():
+    def __init__(self, fenstring):
+        self.moves_without_caputres = 0
+        self.grid = [[]]
+        pieces = {
+            'r':Rook(Team.black,self),
+            'n':Knight(Team.black,self),
+            'b':Bishop(Team.black,self),
+            'q':Queen(Team.black,self),
+            'k':King(Team.black,self),
+            'p':Pawn(Team.black,self),
 
+            'R':Rook(Team.white,self),
+            'N':Knight(Team.white,self),
+            'B':Bishop(Team.white,self),
+            'Q':Queen(Team.white,self),
+            'K':King(Team.white,self),
+            'P':Pawn(Team.white,self)
+        }
+        state = fenstring.split(' ')
+        board_string = state[0]
+        x = y = 0
+        for i in board_string:
+            print(i)
+            if i == '/':
+                x =  0
+                y += 1
+                self.grid.append([])
+            else:
+                try:
+                    space_count = int(i)
+                    for _ in range((space_count)):
+                        self.grid[-1].append(Space(Empty(), (x,y)))
+                        x += 1
+                except(ValueError):
+                    self.grid[-1].append(Space(pieces[i], (x,y)))
+                
+
+        if state[1] == 'w': self.current_player = Team.white 
+        elif state[1] == 'b': self.current_player = Team.black
+        
+        self.moves = int(state[-1])
+        
     def is_valid(self, move):
         #check correct player is moving correct piece
         #check is not same player playing twice
         #check is not putting their king in check
         #check is not a draw
         return True
-    
+        
+    def get_current_player(self):
+        return self.current_player
+
+    def set_current_player(self, next_player):
+        self.current_player = next_player
+
     def execute(self, move):
         pass
     
     def printBoard(self):
-        # print("Move: "+ str(self.moveCount))
-        print('   a  b  c  d  e  f  g  h')
-        rows = ['a','b','c','d','e','f','g','h']
+        """
+        Returns a string representation of the board
+        """
+        print('  | a | b | c | d | e | f | g | h |')
+        print('--|-------------------------------|--')
         count = 8
         for i in self.grid:
-            print(count, end='')
-            count -=1
+            print(str(count), end=' |')
+            
             for j in i:
-                print(' ' + str(j), end='')
-            print()
+                print('' +' \033[1m' + str(j)+'\033[0m ', end='|')
+            print(' '+str(count),'\n--|-------------------------------|--')
+            count -=1
+        print('  | a | b | c | d | e | f | g | h |')
 
-    def getSpot():
-        pass
+    def getSpace(self, position):
+        return self.board[position[0]][position[1]]
+        
 
     def checkMate():
         return False
--- main.py
+++ main.py
@@ -1,28 +1,49 @@
-from chess import Board, Move, Turn
+from chess import Board, Move, Team
 # from logging import *
 
-def gameLoop():
-    game = Board()
-    currentTurn = Turn.white
+def gameLoop(white,black):
+    game = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    next_turn={ Team.white:Team.black,
+            Team.black:Team.white
+    }
+
+    get_move={
+        Team.white:user_input,
+        Team.black:user_input
+    }
+
     while Board.checkMate() == False:
+        # clear old board
         print(chr(27) + "[2J")
+        print('Player turn: ', game.get_current_player(), '\n')
+        # print new board
         game.printBoard()
-        move = parseInput(input(), )
+        print('Next Move: ', end='')
+        # get next move
+        move = get_move[game.current_player]()
         if game.is_valid(move):
             game.execute(move)
-            game.moves.append(move)
-
+            # game.moves.append(move)
+        else:
+            continue
         print(game.moves)
+        game.set_current_player(next_turn[game.get_current_player()])
 
-    return "winner"
+    return "Winner: " + game.get_current_player()
 
+def bot_input():
+    pass
 
-def parseInput(str_note):
+def user_input():
+    str_note = input()
     return Move(str_note)
 
 
+
 def main():
-    gameLoop()
+    white=0
+    black=1
+    gameLoop(white, black)
 
 
 if __name__ == '__main__':
